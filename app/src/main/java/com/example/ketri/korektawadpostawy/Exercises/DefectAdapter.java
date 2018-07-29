46acfee17c2066b9cb8ee163a5642b97d1e99eca
kb861 <katabie861@student.polsl.pl>
2018-07-29 14:31:46 +0200

    values to resources

--- app/src/main/java/com/example/ketri/korektawadpostawy/Exercises/DefectAdapter.java
+++ app/src/main/java/com/example/ketri/korektawadpostawy/Exercises/DefectAdapter.java
@@ -1,13 +1,40 @@
 package com.example.ketri.korektawadpostawy.Exercises;
 
+import android.content.Context;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import com.example.ketri.korektawadpostawy.R;
+
+import java.util.List;
+
+import butterknife.BindView;
 
 /**
  * Created by ketri on 29.07.2018.
  */
 
 public class DefectAdapter extends RecyclerView.Adapter<DefectAdapter.ViewHolder>{
+
+    private Context mContext ;
+    private List<Defect> mData ;
+
+    @BindView(R.id.txt_defect)
+    TextView txt_defect;
+
+    @BindView(R.id.img)
+    ImageView img;
+
+
+
+    public DefectAdapter(Context mContext, List<Defect> mData) {
+        this.mContext = mContext;
+        this.mData = mData;
+    }
+
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         return null;
@@ -24,5 +51,8 @@ public class DefectAdapter extends RecyclerView.Adapter<DefectAdapter.ViewHolder
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        public ViewHolder(View itemView) {
+            super(itemView);
+        }
     }
 }
