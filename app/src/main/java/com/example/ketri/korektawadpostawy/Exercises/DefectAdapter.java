58529038ebf58f67d66afcbea330ac3dad4a65b1
kb861 <katabie861@student.polsl.pl>
2018-07-29 14:42:45 +0200

    small changes

--- app/src/main/java/com/example/ketri/korektawadpostawy/Exercises/DefectAdapter.java
+++ app/src/main/java/com/example/ketri/korektawadpostawy/Exercises/DefectAdapter.java
@@ -1,7 +1,9 @@
 package com.example.ketri.korektawadpostawy.Exercises;
 
 import android.content.Context;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
@@ -12,6 +14,7 @@ import com.example.ketri.korektawadpostawy.R;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 /**
  * Created by ketri on 29.07.2018.
@@ -37,22 +40,75 @@ public class DefectAdapter extends RecyclerView.Adapter<DefectAdapter.ViewHolder
 
     @Override
     public ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        return null;
+        View view ;
+
+        LayoutInflater mInflater = LayoutInflater.from(mContext);
+
+        view = mInflater.inflate(R.layout.item,parent,false);
+        ButterKnife.bind(this,view);
+        return new ViewHolder(view);
     }
 
     @Override
     public void onBindViewHolder(ViewHolder holder, int position) {
+        holder.txt_defect.setText(mData.get(position).getName());
 
+        holder.img.setImageResource(mData.get(position).getImage());
+        holder.cardView.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+
+            public void onClick(View v) {
+
+
+//                if(position==0) {
+//                    Intent intent = new Intent(mContext, ScoliosisExerciseList.class);
+//                    intent.putExtra("Name", mData.get(position).getName());
+//                    mContext.startActivity(intent);
+//                }
+//
+//                if(position==1) {
+//                    Intent intent = new Intent(mContext, KyphosisExerciseList.class);
+//                    intent.putExtra("NameKyphosis", mData.get(position).getName());
+//                    mContext.startActivity(intent);
+//                }
+//
+//                if(position==2) {
+//                    Intent intent = new Intent(mContext, LordosisExerciseList.class);
+//                    intent.putExtra("NameLordosis", mData.get(position).getName());
+//                    mContext.startActivity(intent);
+//                }
+//
+//                if(position==3) {
+//                    Intent intent = new Intent(mContext, FlatExerciseList.class);
+//                    intent.putExtra("NameFlat", mData.get(position).getName());
+//                    mContext.startActivity(intent);
+//                }
+//                if(position==4) {
+//                    Intent intent = new Intent(mContext, RoundExerciseList.class);
+//                    intent.putExtra("NameRound", mData.get(position).getName());
+//                    mContext.startActivity(intent);
+//                }
+
+            }
+
+        });
     }
 
     @Override
     public int getItemCount() {
-        return 0;
+        return mData.size();
     }
 
     public class ViewHolder extends RecyclerView.ViewHolder {
+        TextView txt_defect;
+        ImageView img;
+        CardView cardView;
         public ViewHolder(View itemView) {
             super(itemView);
+            txt_defect = (TextView) itemView.findViewById(R.id.txt_defect) ;
+            img = (ImageView) itemView.findViewById(R.id.img);
+            cardView = (CardView) itemView.findViewById(R.id.card_view);
         }
     }
 }
