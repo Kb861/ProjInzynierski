cb00935144768b53676996909a27b69b07938cfd
kb861 <katabie861@student.polsl.pl>
2018-07-29 18:35:45 +0200

    list of defects

---
+++ app/src/main/java/com/example/ketri/korektawadpostawy/Exercises/DefectActivity.java
@@ -0,0 +1,15 @@
+package com.example.ketri.korektawadpostawy.Exercises;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.ketri.korektawadpostawy.R;
+
+public class DefectActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_defect);
+    }
+}
--- app/src/main/java/com/example/ketri/korektawadpostawy/Exercises/ExerciseFragment.java
+++ app/src/main/java/com/example/ketri/korektawadpostawy/Exercises/ExerciseFragment.java
@@ -3,6 +3,7 @@ package com.example.ketri.korektawadpostawy.Exercises;
 
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,9 +11,11 @@ import android.view.ViewGroup;
 
 import com.example.ketri.korektawadpostawy.R;
 
+import java.util.ArrayList;
 import java.util.List;
 
 import butterknife.BindView;
+import butterknife.ButterKnife;
 
 
 /**
@@ -32,7 +35,21 @@ public class ExerciseFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_exercise, container, false);
-    }
+      View view = inflater.inflate(R.layout.fragment_exercise, container, false);
+        ButterKnife.bind(this, view);
+        lstDefect = new ArrayList<>();
+        lstDefect.add(new Defect("Skolioza", R.drawable.skolioza));
+        lstDefect.add( new Defect("Kifoza", R.drawable.kif));
+        lstDefect.add( new Defect("Lordoza", R.drawable.lo));
+        lstDefect.add( new Defect("Plecy płaskie", R.drawable.plaskie));
+        lstDefect.add( new Defect("Plecy okrągło-wklęsłe", R.drawable.okw));
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity().getApplicationContext());
+        recycler.setLayoutManager(layoutManager);
 
+        DefectAdapter defectAdaper = new DefectAdapter(getContext(),lstDefect);
+        recycler.setAdapter(defectAdaper);
+
+        return view;
+
+    }
 }
