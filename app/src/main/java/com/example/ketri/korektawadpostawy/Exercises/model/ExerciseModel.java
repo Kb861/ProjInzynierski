42cd9333315d9ee2f5fa88e031b82e08387a44c5
kb861 <katabie861@student.polsl.pl>
2018-07-29 19:21:31 +0200

    preparing list of exercises

---
+++ app/src/main/java/com/example/ketri/korektawadpostawy/Exercises/model/ExerciseModel.java
@@ -0,0 +1,19 @@
+package com.example.ketri.korektawadpostawy.Exercises.model;
+
+/**
+ * Created by ketri on 29.07.2018.
+ */
+
+public class ExerciseModel {
+    private String name ;
+
+    private String description;
+
+    private String rating ;
+
+    private String image_url;
+
+    private String categorie;
+
+
+}
