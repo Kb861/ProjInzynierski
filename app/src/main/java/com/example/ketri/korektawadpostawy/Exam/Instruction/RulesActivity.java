98061a69699b7fb2290d61d7a46a36bdcfb56353
kb861 <katabie861@student.polsl.pl>
2018-11-18 17:43:20 +0100

    instruction ready

--- app/src/main/java/com/example/ketri/korektawadpostawy/Exam/Instruction/RulesActivity.java
+++ app/src/main/java/com/example/ketri/korektawadpostawy/Exam/Instruction/RulesActivity.java
@@ -1,15 +1,55 @@
 package com.example.ketri.korektawadpostawy.Exam.Instruction;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
+import com.example.ketri.korektawadpostawy.BackArrowToHome;
+import com.example.ketri.korektawadpostawy.Exam.Examination.ExaminationActivity;
+import com.example.ketri.korektawadpostawy.Exercises.ExerciseFragment;
+import com.example.ketri.korektawadpostawy.Home.activity.InfoActivity;
 import com.example.ketri.korektawadpostawy.R;
 
-public class RulesActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import butterknife.OnClick;
+
+public class RulesActivity extends AppCompatActivity implements BackArrowToHome{
+
+    @BindView(R.id.btn_goexam)
+    Button btn_goexam;
+
+    @OnClick(R.id.btn_goexam)
+    void onClick(View view) {
+        Intent intent = new Intent(this, ExaminationActivity.class);
+        Bundle bundle = new Bundle();
+        intent.putExtras(bundle);
+        startActivity(intent);
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rules);
+        ButterKnife.bind(this);
+        SupportActionBarBack();
+    }
+
+    @Override
+    public void SupportActionBarBack() {
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if(id == android.R.id.home)
+        {
+            this.finish();
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
